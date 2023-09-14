ed13ab3e4017148a0ec68d2596bebf6f8bc47ed2
Martin Ortbauer <mortbauer@gmail.com>
2023-09-14 21:22:52 +0200

    fix cmake for ulp binary intergration

--- ports/esp32/esp32_ulp.c
+++ ports/esp32/esp32_ulp.c
@@ -132,7 +132,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_3(esp32_ulp_set_wakeup_period_obj, esp32_ulp_set_
     }
     STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_ulp_riscv_run_obj, esp32_ulp_riscv_run);
 
-    #include "genhdr/ulp_main.h"
+    #include "ulp_genhdr/ulp_my_main.h"
     STATIC mp_obj_t esp32_ulp_riscv_get_var(mp_obj_t self_in) {
         return MP_OBJ_NEW_SMALL_INT(ulp_spi_result);
     }
@@ -141,14 +141,14 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_3(esp32_ulp_set_wakeup_period_obj, esp32_ulp_set_
 
 
 #if (CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3) && CONFIG_ULP_COPROC_TYPE_RISCV
-    #include "genhdr/esp32_ulpconst_qstr.h"
+    #include "ulp_genhdr/esp32_ulpconst_qstr.h"
 #endif
 
 STATIC const mp_rom_map_elem_t esp32_ulp_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_set_wakeup_period), MP_ROM_PTR(&esp32_ulp_set_wakeup_period_obj) },
     { MP_ROM_QSTR(MP_QSTR_RESERVE_MEM), MP_ROM_INT(CONFIG_ULP_COPROC_RESERVE_MEM) },
     #if (CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3) && CONFIG_ULP_COPROC_TYPE_RISCV
-    #include "genhdr/esp32_ulpconst_mpz.h"
+    #include "ulp_genhdr/esp32_ulpconst_mpz.h"
     { MP_ROM_QSTR(MP_QSTR_riscv_load_default_binary), MP_ROM_PTR(&esp32_ulp_riscv_load_default_binary_obj) },
     { MP_ROM_QSTR(MP_QSTR_riscv_load_binary), MP_ROM_PTR(&esp32_ulp_riscv_load_binary_obj) },
     { MP_ROM_QSTR(MP_QSTR_riscv_run), MP_ROM_PTR(&esp32_ulp_riscv_run_obj) },
