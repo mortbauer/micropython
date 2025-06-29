1bc376385f0b331410c4571e60e80167080f3db5
Martin Ortbauer <mortbauer@gmail.com>
2025-06-29 05:39:07 +0200

    make webrel actually possible to disable for esp32

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -183,7 +183,9 @@
 #define MICROPY_PY_SSL                      (1)
 #define MICROPY_SSL_MBEDTLS                 (1)
 #define MICROPY_PY_WEBSOCKET                (1)
-#define MICROPY_PY_WEBREPL                  (1)
+#ifndef MICROPY_PY_WEBREPL
+#define MICROPY_PY_WEBREPL (1)
+#endif
 #define MICROPY_PY_ONEWIRE                  (1)
 #define MICROPY_PY_SOCKET_EVENTS            (MICROPY_PY_WEBREPL)
 #define MICROPY_PY_BLUETOOTH_RANDOM_ADDR    (1)
